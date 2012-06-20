e04d179a5f8dcb09249161570f705f50829429b9
Scott Mayo <scott.mayo@northgorge.com>
2012-06-20 14:30:28 +1200

    Renamed time_diff for accuracy

--- test/helper.rb
+++ test/helper.rb
@@ -15,7 +15,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'time_diff'
+require 'time_extensions'
 require 'timespan'
 
 class Test::Unit::TestCase
